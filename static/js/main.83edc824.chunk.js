c8584d3241f4f0b612d8c98dc278dc9881ea66f4
Code Sandbox <hello@codesandbox.io>
2022-04-11 14:12:55 +0000

    Deployment from Codesandbox.

--- static/js/main.83edc824.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptodolist=this.webpackJsonptodolist||[]).push([[0],{12:function(e,t,c){},14:function(e,t,c){"use strict";c.r(t);var o=c(1),n=c(3),s=(c(12),c(0));function i(){return Object(s.jsxs)("div",{className:"App",children:[Object(s.jsx)("h1",{children:"Hello CodeSandbox"}),Object(s.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var r=document.getElementById("root");Object(n.createRoot)(r).render(Object(s.jsx)(o.StrictMode,{children:Object(s.jsx)(i,{})}))}},[[14,1,2]]]);
-//# sourceMappingURL=main.83edc824.chunk.js.map
---
+++ static/js/main.8a170004.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonptodolist=this.webpackJsonptodolist||[]).push([[0],[function(o,s){}],[[0,1]]]);
+//# sourceMappingURL=main.8a170004.chunk.js.map
