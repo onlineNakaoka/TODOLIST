81057575d3501447d30d4775d4c523c553a9b213
Code Sandbox <hello@codesandbox.io>
2022-04-11 14:11:56 +0000

    Deployment from Codesandbox.

--- static/js/main.45b96299.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonptodo=this.webpackJsonptodo||[]).push([[0],{12:function(e,t,c){},14:function(e,t,c){"use strict";c.r(t);var o=c(1),n=c(3),s=(c(12),c(0));function r(){return Object(s.jsxs)("div",{className:"App",children:[Object(s.jsx)("h1",{children:"Hello CodeSandbox"}),Object(s.jsx)("h2",{children:"Start editing to see some magic happen!"})]})}var i=document.getElementById("root");Object(n.createRoot)(i).render(Object(s.jsx)(o.StrictMode,{children:Object(s.jsx)(r,{})}))}},[[14,1,2]]]);
-//# sourceMappingURL=main.45b96299.chunk.js.map
